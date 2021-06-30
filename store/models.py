7abcafede284f03149650cbaf2d9953a8920aea5
Jahongir <jahongirnagmatov@gmail.com>
2021-06-30 12:08:25 +0500

    add collection and order_item models

--- store/models.py
+++ store/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -25,22 +29,29 @@ class Customer(models.Model):
     phone = models.CharField(max_length=50)
     birth_date=models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
 
 
 class Order(models.Model):
     STATUS_PENDING = 'P'
     STATUS_COMPLETE = 'C'
     STATUS_FAILED = 'F'
-    PAYMENT_CHOICES = [
+    PAYMENT_STATUS_CHOICES = [
         (STATUS_PENDING, 'Pending')
         (STATUS_COMPLETE, 'Complete')
         (STATUS_FAILED, 'Failed')
     ]
     placed_at = models.DateTimeField(auto_now=True)
-    payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=STATUS_PENDING)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT) # models.PROTECT -> if customer is deleted, Orders will not be deleted
 
+class OrderItem(models.Model):
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE) #One to one relationship added here Customer - Address. on_delete = models.CASCADE - deletes the data when customer is deleted
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True) #One to one relationship added here Customer - Address. on_delete = models.CASCADE - deletes t`he data when customer is deleted
