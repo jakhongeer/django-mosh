02ef24ed27f71a0da615cdda59cef8b3144abd39
Jahongir <jahongirnagmatov@gmail.com>
2021-06-30 11:50:18 +0500

    add order model

--- store/models.py
+++ store/models.py
@@ -25,3 +25,22 @@ class Customer(models.Model):
     phone = models.CharField(max_length=50)
     birth_date=models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    STATUS_PENDING = 'P'
+    STATUS_COMPLETE = 'C'
+    STATUS_FAILED = 'F'
+    PAYMENT_CHOICES = [
+        (STATUS_PENDING, 'Pending')
+        (STATUS_COMPLETE, 'Complete')
+        (STATUS_FAILED, 'Failed')
+    ]
+    placed_at = models.DateTimeField(auto_now=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=STATUS_PENDING)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE) #One to one relationship added here Customer - Address. on_delete = models.CASCADE - deletes the data when customer is deleted
