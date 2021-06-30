66d3d39762d0c7a3a9e0f93d46233f74ac1c28e5
Jahongir <jahongirnagmatov@gmail.com>
2021-06-30 12:36:24 +0500

    add promotion model in Store app

--- store/models.py
+++ store/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+') #when Product is deleted this will be setted null
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -11,7 +17,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE) #ForeignKey - is used to do one to many relationships between tables
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
